d6d7c4f1ce252a94d18f1421425ad8969a75c2f5
Thaistranf <anhnamduong9x9@gmail.com>
2024-07-26 01:25:20 +0700

    design message

--- lib/features/message/screens/message_home_screen.dart
+++ lib/features/message/screens/message_home_screen.dart
@@ -1,32 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tomiru_social_flutter/screens/message/screens/list_chat_screen.dart';
-import 'package:tomiru_social_flutter/screens/message/screens/list_group_chat_screen.dart';
-import 'package:tomiru_social_flutter/screens/message/widgets/custom_app_bar_widget.dart';
-import 'package:tomiru_social_flutter/screens/message/widgets/custom_popup_menu_button_widget.dart';
-import 'package:tomiru_social_flutter/screens/message/widgets/custom_inline_widget.dart';
-import 'package:tomiru_social_flutter/screens/message/widgets/custom_text_field_widget.dart';
-import 'package:tomiru_social_flutter/screens/message/screens/group_creation_screen.dart';
-import 'package:tomiru_social_flutter/screens/message/screens/new_message_screen.dart';
-import 'package:tomiru_social_flutter/screens/message/screens/group_calendar_creation_screen.dart';
-import 'package:tomiru_social_flutter/screens/message/widgets/custom_color_widget.dart'
+import 'package:tomiru_social_flutter/features/message/screens/list_chat_screen.dart';
+import 'package:tomiru_social_flutter/features/message/screens/list_group_chat_screen.dart';
+import 'package:tomiru_social_flutter/features/message/widgets/custom_app_bar_widget.dart';
+import 'package:tomiru_social_flutter/features/message/widgets/custom_popup_menu_button_widget.dart';
+import 'package:tomiru_social_flutter/features/message/widgets/custom_inline_widget.dart';
+import 'package:tomiru_social_flutter/features/message/widgets/custom_text_field_widget.dart';
+import 'package:tomiru_social_flutter/features/message/screens/group_creation_screen.dart';
+import 'package:tomiru_social_flutter/features/message/screens/new_message_screen.dart';
+import 'package:tomiru_social_flutter/features/message/screens/group_calendar_creation_screen.dart';
+import 'package:tomiru_social_flutter/features/message/widgets/custom_color_widget.dart'
     as CustomAppColor;
 
-class MessageList extends StatefulWidget {
+class MessageList extends StatelessWidget {
   const MessageList({super.key});
 
   @override
-  State<MessageList> createState() => _MessageListState();
-}
-
-class _MessageListState extends State<MessageList> {
-  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: PreferredSize(
-          preferredSize: Size.fromHeight(65.0),
+          preferredSize: Size.fromHeight(size.height / 13.6),
           child: CustomAppbar(
             title: 'Tin nhắn',
             redirect: () {},
@@ -53,7 +49,7 @@ class _MessageListState extends State<MessageList> {
             CustomInline(width: double.infinity),
             Container(
               width: double.infinity,
-              height: 45,
+              height: size.height / 19.8,
               color: CustomAppColor.backgroundTabBar,
               child: TabBar(
                 indicatorColor: CustomAppColor.indicatorTabBar,
--- lib/features/message/widgets/custom_group_chat_avatar_widget.dart
+++ lib/features/message/widgets/custom_group_chat_avatar_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tomiru_social_flutter/screens/message/widgets/custom_color_widget.dart'
+import 'package:tomiru_social_flutter/features/message/widgets/custom_color_widget.dart'
     as CustomAppColor;
 
 class CustomGroupChatAvatar extends StatelessWidget {
@@ -18,17 +18,18 @@ class CustomGroupChatAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Container(
-      width: 45,
-      height: 45,
+      width: size.width / 9.05,
+      height: size.width / 9.05,
       color: CustomAppColor.backgroundAvatarGroupChat,
       child: Stack(
         children: [
           Positioned(
-            top: 21,
-            left: 20.9,
+            top: size.height / 41.5,
+            left: size.width / 19.3,
             child: Container(
-              padding: EdgeInsets.all(0.8),
+              padding: EdgeInsets.all(0.9),
               decoration: BoxDecoration(
                 color: CustomAppColor.pictureFrame,
                 shape: BoxShape.circle,
@@ -36,17 +37,17 @@ class CustomGroupChatAvatar extends StatelessWidget {
               child: ClipOval(
                 child: Image.network(
                   image1,
-                  width: 22.5,
-                  height: 22.5,
+                  width: size.width / 18.4,
+                  height: size.height / 40,
                   fit: BoxFit.cover,
                 ),
               ),
             ),
           ),
           Positioned(
-            top: 21,
+            top: size.height / 41.5,
             child: Container(
-              padding: EdgeInsets.all(0.8),
+              padding: EdgeInsets.all(0.9),
               decoration: BoxDecoration(
                 color: CustomAppColor.pictureFrame,
                 shape: BoxShape.circle,
@@ -54,17 +55,17 @@ class CustomGroupChatAvatar extends StatelessWidget {
               child: ClipOval(
                 child: Image.network(
                   image2,
-                  width: 22.5,
-                  height: 22.5,
+                  width: size.width / 18.4,
+                  height: size.height / 40,
                   fit: BoxFit.cover,
                 ),
               ),
             ),
           ),
           Positioned(
-            left: 20.9,
+            left: size.width / 19.3,
             child: Container(
-              padding: EdgeInsets.all(0.8),
+              padding: EdgeInsets.all(0.9),
               decoration: BoxDecoration(
                 color: CustomAppColor.pictureFrame,
                 shape: BoxShape.circle,
@@ -72,15 +73,15 @@ class CustomGroupChatAvatar extends StatelessWidget {
               child: ClipOval(
                 child: Image.network(
                   image3,
-                  width: 22.5,
-                  height: 22.5,
+                  width: size.width / 18.4,
+                  height: size.height / 40,
                   fit: BoxFit.cover,
                 ),
               ),
             ),
           ),
           Container(
-            padding: EdgeInsets.all(0.8),
+            padding: EdgeInsets.all(0.9),
             decoration: BoxDecoration(
               color: CustomAppColor.pictureFrame,
               shape: BoxShape.circle,
@@ -88,8 +89,8 @@ class CustomGroupChatAvatar extends StatelessWidget {
             child: ClipOval(
               child: Image.network(
                 image4,
-                width: 22.5,
-                height: 22.5,
+                width: size.width / 18.4,
+                height: size.height / 40,
                 fit: BoxFit.cover,
               ),
             ),
