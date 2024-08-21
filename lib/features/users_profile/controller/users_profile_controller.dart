408630144cb8daa60adc681505e2673154e820f6
khanh <khanhlnfx20736@funix.edu.vn>
2024-08-21 17:06:19 +0700

    qr scan

--- lib/features/users_profile/controller/users_profile_controller.dart
+++ lib/features/users_profile/controller/users_profile_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get_state_manager/src/rx_flutter/rx_disposable.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:tomiru_social_flutter/features/users_profile/domain/models/users_me.dart';
 
 import '../domain/service/users_profile_service_interface.dart';
 
@@ -8,14 +9,16 @@ class UsersProfileController extends GetxController implements GetxService {
   UsersProfileController({required this.userProfileServiceInterface}) {
     setCurrentUsers();
   }
-
+  UserProfile? _userProfile;
+  UserProfile? get userProfile => _userProfile;
   void setCurrentUsers() async {
     await userProfileServiceInterface.fetchCurrentUsers();
   }
 
   void getCurrentUsersLocal() async {
-   final userProfile = await userProfileServiceInterface.getCurrentUsersLocal();
-   print(userProfile.firstName);
+    UserProfile res = await userProfileServiceInterface.getCurrentUsersLocal();
+
+    _userProfile = res;
   }
 
   void setMasterData() async {
@@ -30,7 +33,8 @@ class UsersProfileController extends GetxController implements GetxService {
   }
 
   void getUsersBalancesLocal() async {
-    final usersBalances = await userProfileServiceInterface.getUsersBalancesLocal();
+    final usersBalances =
+        await userProfileServiceInterface.getUsersBalancesLocal();
     print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1');
     print(usersBalances);
     print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1');
