5e7ad3fc77a23696762df517f979c71dee764e93
khanh <khanhlnfx20736@funix.edu.vn>
2024-08-23 13:51:10 +0700

    asd

--- lib/features/bussiness/domain/repositories/business_repo.dart
+++ lib/features/bussiness/domain/repositories/business_repo.dart
@@ -1,6 +1,7 @@
 import 'package:tomiru_social_flutter/api/api_client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tomiru_social_flutter/features/bussiness/domain/models/wallet_info.dart';
+import 'package:tomiru_social_flutter/features/bussiness/domain/models/packages.dart';
 import 'package:tomiru_social_flutter/features/bussiness/domain/repositories/business_repo_interface.dart';
 import 'package:tomiru_social_flutter/util/app_constants.dart';
 import 'package:tomiru_social_flutter/features/bussiness/domain/models/vault_info.dart';
@@ -28,15 +29,31 @@ class BusinessRepo implements BusinessRepoInterface {
       throw Exception('errors: ${response.statusText}');
     }
   }
+
   @override
   Future<List<WalletInfo>> getWalletInfo() async {
-    Response response = await apiClient.getData(AppConstants.apiV1UsersWalletHistory);
+    Response response =
+        await apiClient.getData(AppConstants.apiV1UsersWalletHistory);
     if (response.statusCode == 200) {
       List<dynamic> dataList = response.body['data'];
       List<WalletInfo> walletInfoList =
           dataList.map((json) => WalletInfo.fromJson(json)).toList();
-          
+
       return walletInfoList;
+    } else {
+      throw Exception('errors: ${response.statusText}');
+    }
+  }
+
+  @override
+  Future<List<PackagesAvailable>> getPackages() async {
+    Response response =
+        await apiClient.getData(AppConstants.apiV1UsersPackages);
+    if (response.statusCode == 200) {
+      List<dynamic> dataList = response.body['data'].packages;
+      List<PackagesAvailable> packagesList =
+          dataList.map((json) => PackagesAvailable.fromJson(json)).toList();
+      return packagesList;
     } else {
       throw Exception('errors: ${response.statusText}');
     }
