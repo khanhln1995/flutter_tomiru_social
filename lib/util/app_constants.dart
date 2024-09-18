f07ba88bdfafe1b87e77185fec109bd7295166a9
daijja <buiminh8690@gmail.com>
2024-09-18 16:55:12 +0700

    color

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -11,7 +11,7 @@ class AppConstants {
   // static const String baseUrl = 'http://192.168.102.65:8000';
   //! Link MXH
   // static const String baseUrl = 'http://192.168.102.132:8000';
-  static const String baseUrl = 'http://192.168.102.53:3003';
+  static const String baseUrl = 'http://192.168.102.56:3003';
   static const String socialUrl = 'http://192.168.102.34:8000';
   // sentTOken
 
